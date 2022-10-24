7d989bfaf76cc0f5a1fdfced1e2fb7831e98e9d2
최윤주 <yeomju311@gmail.com>
2022-10-25 00:17:55 +0900

    andThen(), compose(), and(), or(), negate()

---
+++ JAVA_PART5_람다식_자바FX1/sec02/example07/Address.java
@@ -0,0 +1,30 @@
+package sec02.example07;
+
+public class Address {
+
+	private String country;
+	private String city;
+	
+	public Address(String country, String city) {
+		super();
+		this.country = country;
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+}
---
+++ JAVA_PART5_람다식_자바FX1/sec02/example07/People.java
@@ -0,0 +1,40 @@
+package sec02.example07;
+
+public class People {
+
+	private String name;
+	private String id;
+	private Address address;
+	
+	public People(String name, String id, Address address) {
+		super();
+		this.name = name;
+		this.id = id;
+		this.address = address;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+}
